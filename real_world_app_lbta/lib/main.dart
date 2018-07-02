ded0a83b178fa51fc2c23717fbf8bc0e4d13dd98
hyunsoo_han <hyunsu_han@fursys.com>
2018-07-02 17:38:25 +0900

    setting RealWorldState and getting json data from url

--- real_world_app_lbta/lib/main.dart
+++ real_world_app_lbta/lib/main.dart
@@ -1,109 +1,63 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(new MyApp());
+void main() => runApp(new RealWorldApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class RealWorldApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    State<StatefulWidget> createState() {
+      return new RealWorldState();
+    }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class RealWorldState extends State<StatefulWidget> {
+  var _isLoading = true;  // using underscore means private variable
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  _fetchData() async {
+    print("Attempting to fetch data from network");
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    final url = "https://api.letsbuildthatapp.com/youtube/home_feed";
+    final response = await http.get(url);
 
-  final String title;
+    if (response.statusCode == 200) {
+      // print(response.body);
 
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
+      final map = json.decode(response.body);
+      final videosJson = map["videos"];
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+      videosJson.forEach((video) {
+        print(video["name"]);
+      });
+      // print(map["videos"]);
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return new MaterialApp(
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text("REAL WORLD APP BAR"),
+            actions: <Widget>[
+              new IconButton(icon: new Icon(Icons.refresh),
+              onPressed: () {
+                print("Reloading..."); 
+                setState(() {
+                  _isLoading = false;           
+                });
+                _fetchData();
+              },)
+            ],
+          ),
+          body: new Center(
+            child: _isLoading ? new CircularProgressIndicator() 
+            : new Text("Finished Loading!!!!!!!!!!!.."),
+
+          )
+        )
+      );
+    }
 }
